0f8c1a4ee0d5e30e3f2d2fc85dc1693753cc724e
Denis Barishev <denz@twiket.com>
2014-09-17 14:54:07 +0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dennybaa@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures consul'
 long_description 'Installs/Configures consul'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'fig'
 depends 'selfpki'
